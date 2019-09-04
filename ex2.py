92e27bdbfebceb9f81284ab9fee84e1e78c68191
github username <github email address>
2019-09-03 22:09:44 -0400

    fix ex1 and fin ex2

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# A comment, this is so you can read your program later.
+# Anthing after the # is ignored by python.
+
+print "I could have code like this." # an the comment after is ignored
+
+# you can also use a comment to "disable" or comment out a piece of code:
+
+print "this will run."
